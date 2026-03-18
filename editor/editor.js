24cd19210b8ba687887ff09be8e04e74bb22e4d2
Diogo Fernando Trevisan <pancatrevisan@gmail.com>
2026-03-18 16:56:05 -0400

    loader working...

--- editor/editor.js
+++ editor/editor.js
@@ -1,12 +1,42 @@
 class Editor
 {
-    constructor(){
-        this.screens = [];
+    static EDITOR_ID = "PDL_EDITOR";
+
+    constructor(xml=null){
+
+        this.app = null;
+        if(xml == null){
+            this.newApp();
+        }
 
     }
 
-    displayScreen(screen){
-        
+    loadApp(xml){
+
     }
+
+    newApp(){
+        let me = this;
+        let req = new XMLHttpRequest();
+        req.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                let loader = new PDLAppLoader(req.responseText);
+                me.app = loader.loadApp();
+                console.log("recv req...");                console.log(me.app);
+                
+                }
+            
+        };
+        req.open("GET", "xml/empty_template.xml");
+        console.log("send req...");
+        req.send();
+    }
+
+    editScreen(screen){
+        document.getElementById(EDITOR_ID).replaceChildren();
+
+    }
+
+
 
 }
--- utils/Loader.js
+++ utils/Loader.js
@@ -1,13 +1,19 @@
 class Loader{
-    constructor(){
-        this.loadScripts();
+    constructor(prefix=""){
+        this.loadScripts(prefix);
 
     }
 
-    loadScripts(){
-        let    fileList = ['core/PDLData.js', 'core/PDLMedia.js', 'core/PDLScreen.js', 'core/PDLApp.js', 'command/CommandCenter.js'];
-        for(file in fileList){
-            
+    loadScripts(prefix=""){
+        let    fileList = ['command/CommandCenter.js','core/PDLData.js', 'core/PDLScreen.js','core/PDLMedia.js', 'core/PDLApp.js', 'core/element/PDLElement.js',
+           'core/element/PDLButton.js', 'core/element/PDLDisplayMedia.js','core/element/DisplayData.js','command/Command.js', 'command/NextScreen.js',
+            'core/PDLAppLoader.js'];
+
+        for(let i = 0; i < fileList.length; i++){
+            let file = prefix+fileList[i];
+            let scr = document.createElement('script');
+            scr.src = file;
+            document.head.appendChild(scr);
         }
     }
 }
